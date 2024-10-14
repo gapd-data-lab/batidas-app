1c966ddf092368de035367b8e38dec15a6c70f43
gapd-data-lab <gapd.data.lab@gmail.com>
2024-10-14 08:25:17 -0300

    Updated version of batidas app - improvements in graph, statistics table and in config.yaml

--- batidas.py
+++ batidas.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 from matplotlib.ticker import MaxNLocator, FuncFormatter
 import datetime
 import io
